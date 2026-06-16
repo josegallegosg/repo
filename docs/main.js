692f2bbf4288db0c6e77b8c452b8218ac2be04ed
josegallegosg <josefa.gallegos@uc.cl>
2026-06-16 13:08:09 -0400

    Add scroll reveal functionality in main.js

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    reveals.forEach(section => {
+
+        const top = section.getBoundingClientRect().top;
+
+        if(top < window.innerHeight - 100){
+
+            section.classList.add("active");
+
+        }
+
+    });
+
+});
